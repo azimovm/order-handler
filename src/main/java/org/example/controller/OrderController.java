ab7a22cac96cdb1e893279afdedf0a591a3ea729
Azimov Makhmudjon <Makhmudjon.Azimov@swisscom.com>
2024-03-26 00:18:12 +0200

    adding the Security Classification

--- src/main/java/org/example/controller/OrderController.java
+++ src/main/java/org/example/controller/OrderController.java
@@ -1,17 +1,21 @@
 package org.example.controller;
+
+@Tag(name = "order-management")
 @RestController
 @RequestMapping("/v1")
 public class OrderController {
         @GetMapping("/order-status")
+        @SecurityClassification(dataConfidentiality = DataConfidentiality.C3, dataCompliance = @DataCompliance(ComplianceFlag.RDPP))
         public String getStatus() {
             return "This is an example response from the server.";
         }
         @PostMapping("/submit-order")
+        @SecurityClassification(dataConfidentiality = DataConfidentiality.C3, dataCompliance = @DataCompliance(ComplianceFlag.RDPP))
         public String submitOrder(@RequestBody String requestBody) {
-            // Process the incoming request body
             return "Received the following request body: " + requestBody;
         }
         @GetMapping("/order-availability-Status")
+        @SecurityClassification(dataConfidentiality = DataConfidentiality.C2, dataCompliance = @DataCompliance(ComplianceFlag.RDPP))
         public String getAviability() {
             return "This is an example response from the server.";
         }
