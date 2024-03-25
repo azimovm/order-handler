2e7d43d542a91de0bcab294272fc9bafd475be08
Azimov Makhmudjon <Makhmudjon.Azimov@swisscom.com>
2024-03-26 00:11:50 +0200

    tag-2 changes in controllers

--- src/main/java/org/example/controller/OrderController.java
+++ src/main/java/org/example/controller/OrderController.java
@@ -1,16 +1,19 @@
 package org.example.controller;
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/v1")
 public class OrderController {
         @GetMapping("/order-status")
-        public String getExample() {
+        public String getStatus() {
             return "This is an example response from the server.";
         }
-
         @PostMapping("/submit-order")
-        public String postExample(@RequestBody String requestBody) {
+        public String submitOrder(@RequestBody String requestBody) {
             // Process the incoming request body
             return "Received the following request body: " + requestBody;
+        }
+        @GetMapping("/order-availability-Status")
+        public String getAviability() {
+            return "This is an example response from the server.";
         }
 
 }
